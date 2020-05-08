9ec78eebe81fc5a6772cf77332b017893fa1972e
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-05-08 17:57:05 -0400

    added bst

---
+++ index.js
@@ -0,0 +1,52 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left  = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor(value){
+        this.root  = new Node(value);
+        this.count = 0;
+    }
+
+    size(){
+
+    }
+
+    insert(){
+
+    }
+
+    min(){
+
+    }
+
+    max(){
+
+    }
+
+    contains(){
+
+    }
+
+    // depth first search in order
+    dfsInOrder(){
+
+    }
+     // depth first search pre-order
+    dfsPreOrder(){
+
+    }
+    // depth first search post-order
+    dfsPostOrder(){
+
+    }
+
+    // breadth first search
+    bfs(){
+        
+    }
+}
